b2711ac09ee146daba0f8ad24773d44df17a5323
‘Katie <ktschr11@gmail.com>
2021-12-14 11:09:30 -0800

    add more notes before meeting back with marty

---
+++ dequeue(o1).js
@@ -0,0 +1,18 @@
+// talking it through
+// for loops, and shift are O of n
+// the hint made me think that it might be good to track the first el in the list in state
+// like firstUp = #list[0];
+// but I'm not sure how or if it's possible to reassign indecies 
+//splice is also O of n
+
+class Queue {
+
+
+  #list = [];
+
+
+  dequeue () {
+
+  }
+
+}
